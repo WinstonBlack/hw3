6980e83669e9f1a56bb56a50464c67bf1bfa80a3
Winston Black <35714947+WinstonBlack@users.noreply.github.com>
2018-02-05 20:26:08 -0800

    Create drawing-with-diameter.js

---
+++ drawing-with-diameter.js
@@ -0,0 +1,45 @@
+function setup() {
+	createCanvas(400, 400);
+	colorMode(HSB)
+}
+
+var diameter = 10;
+
+function draw() {
+	if (mouseIsPressed) {
+		ellipse(mouseX, mouseY, diameter);
+	}
+}
+
+function keyPressed() {
+	print(key);
+	if (key == 'P') {
+		fill(280, 100, 100);
+	} 
+	else if (key == 'G') {
+		fill(80, 80, 100);
+	} 
+	else if (key == 'B') {
+		fill(200, 100, 100);
+	}	
+	else if (key == 'O') {
+		fill(30, 100, 100);
+	} 
+	else if (key == 'Z') {
+		fill(30, 100, 0);
+	}	
+	else if (key == 'X') {
+		fill(360, 25, 50);
+	}
+	else if (key == '1') {
+		diameter = diameter+ 10;
+	}	
+	else if (key == '2') {
+		diameter = diameter+ 20;
+	}	  
+else if (key == '3') {
+		diameter = diameter+ 30;
+	}	  
+
+
+}
